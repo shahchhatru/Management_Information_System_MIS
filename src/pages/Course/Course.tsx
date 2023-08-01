7cb92df4602d6b87c60d5a0b0c363636ac8d0bec
chhatra shah <shahkhesavchhatra@gmail.com>
2023-08-01 11:38:20 +0545

    responsive student-card

--- src/pages/Course/Course.tsx
+++ src/pages/Course/Course.tsx
@@ -1,20 +1,51 @@
 import { useParams } from "react-router-dom";
+import {useState,useRef,useEffect} from 'react';
 import './styles.css'
 import { StudentCard } from "../../components";
+import {motion} from 'framer-motion';
 
 const Course = () => {
   let {id} =useParams();
+  const [width, setWidth] = useState<number>(0);
+  const carousel = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (carousel.current) {
+      console.log(carousel.current.scrollWidth, carousel.current.offsetWidth);
+      setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+    }
+  }, []);
   return (
     <div className="course-container">
       <div className="head">
         {id}
       </div>
       <div className="body">
+      <motion.div
+        ref={carousel}
+        className="carousel"
+        whileTap={{ cursor: "grabbing" }}
+      >
+        <motion.div
+          drag="x"
+          dragConstraints={{ right: 0, left: -width }}
+          className="inner-carousel"
+        >
         <StudentCard/>
         <StudentCard/>
+        <StudentCard/>
+        <StudentCard/>
+        <StudentCard/>
+        <StudentCard/>
+        <StudentCard/>
+        <StudentCard/>
+        <StudentCard/>
+        <StudentCard/>
+        </motion.div>
+        </motion.div>
       </div>
     </div>
   )
 }
 
-export default Course
+export default Course;
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,20 @@
 import { AdminOptionCard } from "../../components";
-import "./styles.css";
+import {useState,useRef,useEffect}  from 'react';
 
-const Dashboard = () => {
+import {motion} from 'framer-motion'
+
+import "./styles.css";
+const Dashboard = (): JSX.Element=> {
+  const [width, setWidth] = useState<number>(0);
+  const carousel = useRef<HTMLDivElement>(null);
+  
+  useEffect(() => {
+    if (carousel.current) {
+      console.log(carousel.current.scrollWidth, carousel.current.offsetWidth);
+      setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth +100);
+    }
+  });
+
   return (
     <div className="dashboard-container">
       <div className="header">
@@ -9,8 +22,27 @@ const Dashboard = () => {
       </div>
       <div className="activity-container">
         <h2>Actions</h2>
-        <div className="activity-list">
+        <motion.div 
+           ref={ carousel }
+          className="carousel"
+           whileTap={{ cursor: "grabbing" }}
+        >
+        <motion.div 
+        className="activity-list"
+        drag="x"
+       dragConstraints={{right: 0, left: -width}}
+        >
+        
+
+       
         <AdminOptionCard
+            linkaddr="/register/teacher"
+            Iid="1"
+            imgPath="/office.svg"
+            imgtext="create teacher"
+            title="Register Teacher"
+          />
+           <AdminOptionCard
             linkaddr="/register/teacher"
             Iid="1"
             imgPath="/office.svg"
@@ -24,8 +56,33 @@ const Dashboard = () => {
           imgtext="create student"
           title="Register student"
           />
-        </div>
+          <AdminOptionCard
+            linkaddr="/register/teacher"
+            Iid="1"
+            imgPath="/office.svg"
+            imgtext="create teacher"
+            title="Register Teacher"
+          />
+          <AdminOptionCard
+            linkaddr="/register/teacher"
+            Iid="1"
+            imgPath="/office.svg"
+            imgtext="create teacher"
+            title="Register Teacher"
+          />
+          <AdminOptionCard
+            linkaddr="/register/teacher"
+            Iid="1"
+            imgPath="/office.svg"
+            imgtext="create teacher"
+            title="Register Teacher"
+          />
+        </motion.div>
+        </motion.div>
+      
+
       </div>
+     
       <div className="event-container">
         <h2>Recent Events</h2>
         <div className="event-list">
